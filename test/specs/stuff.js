79f183815f3a1cb47e1e044a13b1e437aa7d8001
Warwick Stone <warwick_stone@fastmail.com>
2021-06-18 23:26:04 +1000

    add percy

--- test/specs/stuff.js
+++ test/specs/stuff.js
@@ -1,3 +1,5 @@
+const percySnapshot = require('@percy/webdriverio');
+
 const baseUrl = 'http://localhost:1313';
 
 async function click(selector) {
@@ -9,11 +11,13 @@ async function click(selector) {
 describe('stuff', () => {
   it('goto homepage', async () => {
     await browser.url(baseUrl);
+    await percySnapshot('homepage');
   });
 
   it('goto blog', async () => {
     await browser.url(baseUrl);
     await click('=Blog');
+    await percySnapshot('/blog');
   });
 });
 
